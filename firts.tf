5186e05f786d3ea0a1ac041835be147ca0bca185
Ved Prakash <41815244+vedprakah3494@users.noreply.github.com>
2024-08-03 19:16:54 +0530

    firstSetup

---
+++ firts.tf
@@ -0,0 +1,6 @@
+output "name" {
+  value = "dffdfd- ${var.myname}"
+}
+output "namelist" {
+  value = "dffdfd- ${var.mynamelist[0]}"
+}
---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "myname" {
+  type = string
+  default="ved prkash "
+  description = "this is my name"
+}
+
+variable "mynamelist" {
+  type = list(string)
+  default=["nnd","dkkkf","dloddo"]
+  description = "this is my name"
+}
